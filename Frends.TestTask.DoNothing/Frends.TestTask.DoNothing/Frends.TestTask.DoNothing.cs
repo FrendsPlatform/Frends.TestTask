7318f7782fa20565ad59982394ac5f61e69deec9
Mateusz Noga-Wojtania <mateusz.noga-wojtania@frends.com>
2026-09-24 12:33:44 +0200

    linter fix

---
+++ Frends.TestTask.DoNothing/Frends.TestTask.DoNothing/Frends.TestTask.DoNothing.cs
@@ -0,0 +1,16 @@
+﻿namespace Frends.TestTask.DoNothing;
+
+/// <summary>
+///  docs
+/// </summary>
+public static class TestTask
+{
+    /// <summary>
+    /// Not a real task.
+    /// </summary>
+    /// <returns>foo</returns>
+    public static bool DoNothing()
+    {
+        return true;
+    }
+}
