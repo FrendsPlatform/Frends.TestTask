3f48e1e590c9dd04656c80dcceb61ce52e232e63
Mateusz Noga-Wojtania <mateusz.noga.wojtania@gmail.com>
2026-09-24 14:26:55 +0200

    coverage

--- Frends.TestTask.DoNothing/Frends.TestTask.DoNothing.Tests/UnitTests.cs
+++ Frends.TestTask.DoNothing/Frends.TestTask.DoNothing.Tests/UnitTests.cs
@@ -18,5 +18,55 @@ namespace Frends.TestTask.DoNothing.Tests
             Assert.IsFalse(result.Success);
             Assert.That(username, Is.EqualTo("bar"));
         }
+
+        [Test]
+        public void DoNothingTest_WithFullyPopulatedParameters()
+        {
+            var input = new Input
+            {
+                Content = "foobar",
+                Repeat = 2,
+            };
+
+            var options = new Options
+            {
+                Delimiter = ",",
+                ThrowErrorOnFailure = false,
+                ErrorMessageOnFailure = "Custom error message",
+            };
+
+            var result = TestTask.DoNothing(input, options, CancellationToken.None);
+
+            Assert.IsFalse(result.Success);
+            Assert.IsNull(result.Error);
+
+            Assert.That(input.Content, Is.EqualTo("foobar"));
+            Assert.That(input.Repeat, Is.EqualTo(2));
+            Assert.That(options.Delimiter, Is.EqualTo(","));
+            Assert.IsFalse(options.ThrowErrorOnFailure);
+            Assert.That(options.ErrorMessageOnFailure, Is.EqualTo("Custom error message"));
+        }
+
+        [Test]
+        public void ResultTest_SuccessAndErrorCanBeSetAndRead()
+        {
+            var exception = new InvalidOperationException("Something went wrong.");
+            var error = new Error
+            {
+                Message = "Unable to join strings.",
+                AdditionalInfo = exception,
+            };
+
+            var result = new Result
+            {
+                Success = true,
+                Error = error,
+            };
+
+            Assert.IsTrue(result.Success);
+            Assert.That(result.Error, Is.Not.Null);
+            Assert.That(result.Error.Message, Is.EqualTo("Unable to join strings."));
+            Assert.That(result.Error.AdditionalInfo, Is.SameAs(exception));
+        }
     }
 }
